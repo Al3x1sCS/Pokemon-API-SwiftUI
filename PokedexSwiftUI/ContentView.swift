0ffd6a4a9288add801c1472d146f388634eef53a
Al3x1s <prog.alexis.cs@protonmail.com>
2022-04-11 12:55:45 -0500

    Implementando a UI

--- PokedexSwiftUI/ContentView.swift
+++ PokedexSwiftUI/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var pokemonModel = PokemonModel()
+    @State private var pokemon = [Pokemon] ()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(pokemon) { poke in
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(poke.name.capitalized)
+                            .font(.title)
+                        Text(poke.type.capitalized)
+                            .italic()
+                        Text(poke.description)
+                            .font(.caption)
+                            .lineLimit(2)
+                    }
+                    AsyncImage(url: URL(string: poke.imageURL)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let Image):
+                            Image.resizable()
+                                .interpolation(.none)
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                        case.failure:
+                            Image(systemName: "photo")
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Pokemon")
+        }
+        
+        .onAppear {
+            async {
+                pokemon = try! await pokemonModel.getPokemon()
+            }
+        }
     }
 }
 

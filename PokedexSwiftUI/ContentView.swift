8fbe7f12181eca0a9061a160bf265d986140b807
Al3x1s <prog.alexis.cs@protonmail.com>
2022-04-12 12:40:35 -0500

    App Finalizado pronto pra implementação de animações e ajustes no Layout

--- PokedexSwiftUI/ContentView.swift
+++ PokedexSwiftUI/ContentView.swift
@@ -10,58 +10,79 @@ import Kingfisher
 
 struct ContentView: View {
     @ObservedObject var pokemonVM = PokemonViewModel()
-    //@State private var pokemon = [Pokemon] ()
+    @State private var searchText = ""
+    
+    var filteredPokemon: [Pokemon] {
+        if searchText == "" { return pokemonVM.pokemon}
+        return pokemonVM.pokemon.filter { $0.name.lowercased().contains(searchText.lowercased())}
+    }
     
     var body: some View {
         NavigationView {
-            List(pokemonVM.pokemon) { poke in
-                NavigationLink(destination: PokemonDetailView(pokemon: poke)) {
-                    
-                    HStack {
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text(poke.name.capitalized)
-                                .font(.title)
-                            HStack {
-                                Text(poke.type.capitalized)
-                                    .italic()
-                                Circle()
-                                    .foregroundColor(poke.typeColor)
-                                    .frame(width: 10, height: 10)
+            List {
+                ForEach(filteredPokemon) { poke in
+                    NavigationLink(destination: PokemonDetailView(pokemon: poke)) {
+                        
+                        HStack {
+                            VStack(alignment: .leading, spacing: 5) {
+                                HStack {
+                                    Text(poke.name.capitalized)
+                                        .font(.title)
+                                    if poke.isFavorite {
+                                        Image(systemName: "star.fill")
+                                            .foregroundColor(.yellow)
+                                    }
+                                }
                                 
+                                
+                                HStack {
+                                    Text(poke.type.capitalized)
+                                        .italic()
+                                    Circle()
+                                        .foregroundColor(poke.typeColor)
+                                        .frame(width: 10, height: 10)
+                                    
+                                }
+                                Text(poke.description)
+                                    .font(.caption)
+                                    .lineLimit(1)
                             }
-                            Text(poke.description)
-                                .font(.caption)
-                                .lineLimit(2)
+                            
+                            Spacer()
+                            // MARK: Kingfisher é lindo
+                            KFImage(URL(string: poke.imageURL))
+                                .interpolation(.none)
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                            
+                            // MARK: metodo assíncrono
+                            /*AsyncImage(url: URL(string: poke.imageURL)) { phase in
+                                switch phase {
+                                case .empty:
+                                    ProgressView()
+                                case .success(let Image):
+                                    Image.resizable()
+                                        .interpolation(.none)
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                case.failure:
+                                    Image(systemName: "photo")
+                                @unknown default:
+                                    EmptyView()
+                                }
+                            }*/
                         }
-                        
-                        Spacer()
-                        // MARK: Kingfisher é lindo
-                        KFImage(URL(string: poke.imageURL))
-                            .interpolation(.none)
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                        
-                        // MARK: metodo assíncrono
-                        /*AsyncImage(url: URL(string: poke.imageURL)) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let Image):
-                                Image.resizable()
-                                    .interpolation(.none)
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                            case.failure:
-                                Image(systemName: "photo")
-                            @unknown default:
-                                EmptyView()
-                            }
-                        }*/
+                    }
+                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button(action: { addFavorite(pokemon: poke)}) {
+                            Image(systemName: "star")
+                        }
+                        .tint(.yellow)
                     }
                 }
-                
             }
             .navigationTitle("Pokemon")
+            .searchable(text: $searchText)
         }
         
         /*.onAppear {
@@ -70,6 +91,12 @@ struct ContentView: View {
                 pokemon = try! await pokemonVM.getPokemon()
             }
         }*/
+    }
+    
+    func addFavorite(pokemon: Pokemon) {
+        if let index = pokemonVM.pokemon.firstIndex(where: {$0.id == pokemon.id }) {
+            pokemonVM.pokemon[index].isFavorite.toggle()
+        }
     }
 }
 
